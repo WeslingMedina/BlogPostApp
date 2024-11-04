bb4fb5bb42dfafec55f7196e117aa57ce943ac91
Wesling Medina <weslingmedina@gmail.com>
2024-11-04 09:43:15 -0600

    Updates

---
+++ precache-manifest.bb2fdcc9b770efe28362bfe968b16415.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "c08ce4bd6d05183028f3",
+    "url": "/BlogPostApp/static/js/0.0f7de9e3.chunk.js"
+  },
+  {
+    "revision": "fed4ea45a8edf0edf1c7",
+    "url": "/BlogPostApp/static/js/1.0ba606b5.chunk.js"
+  },
+  {
+    "revision": "584b7ea55cf0363053c7",
+    "url": "/BlogPostApp/static/js/main.704e152e.chunk.js"
+  },
+  {
+    "revision": "9319bbd4c5e5bfe53383",
+    "url": "/BlogPostApp/static/js/runtime~main.f21260dc.js"
+  },
+  {
+    "revision": "bb47d32bf7ba1365fb34",
+    "url": "/BlogPostApp/static/js/4.13496358.chunk.js"
+  },
+  {
+    "revision": "82eaed399056e6edbe38",
+    "url": "/BlogPostApp/static/js/5.043fd415.chunk.js"
+  },
+  {
+    "revision": "2b46e9958788face950f",
+    "url": "/BlogPostApp/static/js/6.fb6202d8.chunk.js"
+  },
+  {
+    "revision": "083eddcbf860e17f9372",
+    "url": "/BlogPostApp/static/css/7.6a6e1b80.chunk.css"
+  },
+  {
+    "revision": "083eddcbf860e17f9372",
+    "url": "/BlogPostApp/static/js/7.265c9b92.chunk.js"
+  },
+  {
+    "revision": "f6105e01d5232d287bfd",
+    "url": "/BlogPostApp/static/css/8.6bb103c1.chunk.css"
+  },
+  {
+    "revision": "f6105e01d5232d287bfd",
+    "url": "/BlogPostApp/static/js/8.c22cac6a.chunk.js"
+  },
+  {
+    "revision": "f8188542a7cd317f03b4",
+    "url": "/BlogPostApp/static/js/9.6cba8706.chunk.js"
+  },
+  {
+    "revision": "428903df282b47821508",
+    "url": "/BlogPostApp/static/js/10.aa96f2e4.chunk.js"
+  },
+  {
+    "revision": "13679c32ea46114cce11",
+    "url": "/BlogPostApp/static/js/11.ea2bb79b.chunk.js"
+  },
+  {
+    "revision": "b5f839cf3ad45d5e7f56",
+    "url": "/BlogPostApp/static/js/12.f2198338.chunk.js"
+  },
+  {
+    "revision": "8e7fe6339595165f5bca",
+    "url": "/BlogPostApp/static/js/13.f9d2788f.chunk.js"
+  },
+  {
+    "revision": "d0766f0ed574dbd46f02",
+    "url": "/BlogPostApp/static/js/14.9500fdb1.chunk.js"
+  },
+  {
+    "revision": "e7673d3be5bc163c88f2",
+    "url": "/BlogPostApp/static/js/15.60c12ae5.chunk.js"
+  },
+  {
+    "revision": "23eceb5cdcd1140b3f97",
+    "url": "/BlogPostApp/static/js/16.3fda708a.chunk.js"
+  },
+  {
+    "revision": "9c73e4652146cc9014df",
+    "url": "/BlogPostApp/static/js/17.41f68c62.chunk.js"
+  },
+  {
+    "revision": "a8b1daa2d64dc7db61df",
+    "url": "/BlogPostApp/static/js/18.ff4495c0.chunk.js"
+  },
+  {
+    "revision": "2a50f501f0b82790dc8f",
+    "url": "/BlogPostApp/static/js/19.d6fcc69c.chunk.js"
+  },
+  {
+    "revision": "f5f79e9e9b0a336257d2ba9342fdd7ff",
+    "url": "/BlogPostApp/index.html"
+  }
+];
